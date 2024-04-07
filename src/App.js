5c2f87f98379eac5d56541ebdc2f7ff62c9fec71
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-04-07 18:31:49 +0900

    #2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 20 })`
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  span {
+    font-size: 42px;
+    &:hover {
+      font-size: 90px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as={"header"}>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😎</span>
+      </Box>
+    </Wrapper>
   );
 }
 
